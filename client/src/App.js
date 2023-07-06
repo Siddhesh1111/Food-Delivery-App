e14ed050de7805236a2280119b71581b25e90307
schourasia11 <schourasia@greenactionstudio.com>
2023-07-06 14:56:53 -0400

    added Login page, connected to Login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
 import Home from './screens/Home';
+import {
+  Routes,
+  Route,
+  BrowserRouter as Router
+} from "react-router-dom";
+import Login from './screens/Login';
 
 function App() {
   return (
-    <div ><Home/></div>
+    <Router>
+      <div >
+        <Routes>
+          <Route exact path='/' element={<Home/>} />
+          <Route exact path='/login' element={<Login/>} />
+          
+        </Routes>
+      </div>
+    </Router>
+    
   );
 }
 export default App;
