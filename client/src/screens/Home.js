8e1c063fe01a3e816bf7862a0009ba538712bd2e
schourasia11 <schourasia@greenactionstudio.com>
2023-07-07 13:17:19 -0400

    added card

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -1,13 +1,40 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <div>
-      <Navbar/>
-      <div>Body</div>
-      <div><Footer/></div>
+      <Navbar />
+      <div>
+        <div className="card mt-3" style={{ width: "18rem", "maxHeight": "360px" }}>
+          <img src="..." className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              This is the first card
+            </p>
+            <div className="container w-100">
+              <select className="m-2 h-100 w-100 bg-success rounded">
+                {Array.from(Array(6), (e,i)=>{
+                  return (
+                    <option key={i+1} value={i+1}>
+                      {i+1}
+                    </option>
+                  )
+                })}
+              </select>
+              <select className="m-2 h-100 w-100 bg-success rounded">
+                <option value="half">Half</option>
+                <option value="full">Full</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
-  )
+  );
 }
