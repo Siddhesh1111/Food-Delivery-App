93086e78d6dd3d7be22037742fa015137d469e9a
schourasia11 <schourasia@greenactionstudio.com>
2023-07-13 19:51:27 -0400

    minor fix

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 async function mongoDB() {
-    await mongoose.connect('mongodb+srv://siddhesh:siddhesh@cluster0.ulb71ew.mongodb.net/gofoodmern?retryWrites=true&w=majority');
+    const conn = await mongoose.connect('mongodb+srv://siddhesh:siddhesh@cluster0.ulb71ew.mongodb.net/gofoodmern?retryWrites=true&w=majority');
 }
 
 module.exports = mongoDB;
