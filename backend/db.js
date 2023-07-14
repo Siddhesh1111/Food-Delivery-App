4b7ee1452fe27f0d47e3ed9e51871393186c898a
Siddhesh1111 <chourasiasiddhesh11@gmail.com>
2023-07-13 23:10:51 -0400

    made some changes

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 async function mongoDB() {
-    const conn = await mongoose.connect('mongodb+srv://siddhesh:siddhesh@cluster0.ulb71ew.mongodb.net/gofoodmern?retryWrites=true&w=majority');
+    try {
+        const conn = await mongoose.connect('mongodb+srv://siddhesh:siddhesh@cluster0.ulb71ew.mongodb.net/gofoodmern?retryWrites=true&w=majority', {useNewUrlParser: true});
+        if(conn){
+            console.log("MongoDB connected");
+        }
+    } catch (err) {
+        console.log(err);
+    }
+    
 }
 
 module.exports = mongoDB;
