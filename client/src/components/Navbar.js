459f723934e948087543cee0e60e5fddd3fa713b
schourasia11 <schourasia@greenactionstudio.com>
2023-07-19 09:33:12 -0400

    cart updates commit

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,11 +3,12 @@ import { Link, useNavigate } from "react-router-dom";
 import Badge from "react-bootstrap/Badge";
 import Modal from "../Modal";
 import Cart from "../screens/Cart";
+import { useCart } from "./ContextReducer";
 
 export default function Navbar() {
   
   const [cartView, setcartView] = useState(false);
-
+  let data = useCart();
   const navigate = useNavigate();
 
   const handleLogout = () => {
@@ -59,7 +60,7 @@ export default function Navbar() {
               <div>
               <div className="btn bg-white text-success mx-2" onClick={()=>{setcartView(true)}}>
                 My Cart {" "}
-                <Badge pill bg ="danger"> 2 </Badge>
+                <Badge pill bg ="danger"> {data.length} </Badge>
                 </div>
                 {cartView?<Modal onClose={()=>setcartView(false)}><Cart /></Modal>: null}
               <div className="btn bg-white text-danger mx-2" onClick={handleLogout}>Logout</div>
