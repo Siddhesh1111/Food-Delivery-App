89c2086c8580b72205b4b1f8987b194cb75db3fd
schourasia11 <schourasia@greenactionstudio.com>
2023-07-19 11:02:21 -0400

    final commit

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,7 @@ app.use(express.json());
 
 app.use('/api', require("./Routes/CreateUser"));
 app.use('/api', require("./Routes/DisplayData"));
+app.use('/api', require("./Routes/OrderData"));
 
 app.listen(port, () => {
     mongoDB();
